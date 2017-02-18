707d660dde905c79a09caf2569326e1ebec4124d
sonntagsmaerchen <lamprecht.julian@googlemail.com>
2017-02-18 20:58:24 +0100

    mainmenu small change

--- src/mainmenu.cpp
+++ src/mainmenu.cpp
@@ -1,17 +1,21 @@
 #include "mainmenu.h"
 #include "button.h"
+#include "game.h"
 
 void mainMenu(sf::RenderWindow& win) {
-    std::function<void()> start = [](){};
+    std::function<void()> start = [](){
+        
+    };
+
     std::function<void()> options = [](){};
     std::function<void()> quit = [&](){
         win.close();
     };
 
     Button menuButtons[3] = {
-        Button({200,50},std::string("assets/fuck.png"),{200,100}, []() {}),
-        Button({200,50},std::string("assets/fuck.png"),{200,200}, []() {}),
-        Button({200,50},std::string("assets/fuck.png"),{200,300}, []() {})
+        Button({200,50},std::string("assets/fuck.png"),{200,100}, start),
+        Button({200,50},std::string("assets/fuck.png"),{200,200}, options),
+        Button({200,50},std::string("assets/fuck.png"),{200,300}, quit)
     };
 
     while (win.isOpen()) {
@@ -29,7 +33,7 @@ void mainMenu(sf::RenderWindow& win) {
             case sf::Event::MouseButtonPressed:
                 if (event.mouseButton.button == sf::Mouse::Left) {
                     for (int i = 0; i < 3; i++) {
-                        menuButtons[i].clicked(sf::Mouse::getPosition());
+                        menuButtons[i].clicked(sf::Mouse::getPosition(win));
                     }
                 }
             }
