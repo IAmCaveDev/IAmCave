4f8338162bc6e50d46a26fa5317f5d8670156b9e
yuki <yuki77x@googlemail.com>
2017-03-22 14:19:00 +0100

    Add docstrings to Tech and Techtree

--- src/tech.h
+++ src/tech.h
@@ -6,9 +6,15 @@
 
 #include "button.h"
 
+/**
+ * A researchable technology.
+ */
 class Tech {
 private:
     typedef std::vector<std::shared_ptr<Tech>> ParentsVector;
+    /**
+     * The bonuses a technology provides when researched.
+     */
     struct StatBoosts {
         float huntBonus;
         float gatheringBonus;
@@ -29,13 +35,31 @@ private:
     Button button;
 
 public:
+    /**
+     * Constructs a new Tech.
+     * @param path The icon
+     * @param newLevel The level in the Techtree the Tech is in
+     * @param newParents A list of parents
+     */
     Tech(std::string path, short newLevel, ParentsVector newParents);
 
+    /**
+     * @return A reference to the list of parents.
+     */
     ParentsVector& getParents();
 
+    /**
+     * @return A reference to the Tech's button.
+     */
     Button& getButton();
 
+    /**
+     * @return The level of the Tech in the Techtree.
+     */
     short getLevel();
+    /**
+     * Sets the level of the Tech in the Techtree.
+     */
     void setLevel(short newLevel);
 };
 
--- src/techtree.h
+++ src/techtree.h
@@ -11,6 +11,9 @@ using json = nlohmann::json;
 #include "rectangle.h"
 #include "tech.h"
 
+/**
+ * Constructs, stores and positions Techs.
+ */
 class Techtree : public Rectangle {
 private:
     std::map<std::string, std::shared_ptr<Tech>> tree;
@@ -23,17 +26,39 @@ private:
 
     bool visibility;
 
+    /**
+     * Positions the Techs according to the tree's structure.
+     */
     void positionTree(json data, short level, TransformedVector<> lastPos);
+    /**
+     * Parses the json data and constructs new Techs.
+     */
     void parse(std::shared_ptr<Tech> parent, json data, short level);
 
 public:
+    /**
+     * Constructs a Techtree and it's Techs.
+     * @param backgroundPath The image to use as a background.
+     * @param path The path to a json file to load the Techtree data from.
+     * @param newSize The display size of the Techtree.
+     * @param newPos The display position of the Techtree.
+     */
     Techtree(std::string backgroundPath, std::string path,
              TransformedVector<> newSize, TransformedVector<> newPos);
 
+    /**
+     * Sets the visiblity of the Techtree.
+     */
     void setVisibility(bool newVisibility);
 
+    /**
+     * Displays the Techtree and it's Techs.
+     */
     void display(sf::RenderWindow& win);
 
+    /**
+     * Resizes and repositions all Techs in the Techtree.
+     */
     void onResize();
 };
 
