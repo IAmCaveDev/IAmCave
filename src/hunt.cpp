435245f7fca49526fb379a0abc1043d7792f4cd6
microwav3s <microwav3s@googlemail.com>
2017-04-01 04:00:42 +0200

    Add death chance for hunt action

--- src/hunt.cpp
+++ src/hunt.cpp
@@ -1,5 +1,7 @@
 #include "hunt.h"
 
+#include <random>
+
 Hunt::Hunt(bool newIsEasy, short time) : Action(time) {
     isEasy = newIsEasy;
     if (isEasy) {
@@ -17,12 +19,26 @@ void Hunt::addActor(std::shared_ptr<Caveman> newactor) {
 ActionPackage Hunt::resolve() {
     currentDuration += 1;
 
+
     if (currentDuration == totalDuration) {
         short totalFitness = 0;
+        short num_casualties = 0;
+        std::default_random_engine generator;
+        std::uniform_int_distribution<int> distribution(1, 20);
+        auto casualtyDice = std::bind(distribution, generator);
+        int pos = 0;
 
         for (auto& it : actors) {
+            //std::random_device rd;
+            //std::mt19937 rng(rd());
+
+            if (!casualtyDice()) {
+                actors.erase(actors.begin() + pos);
+                it->~Caveman();
+            }
             totalFitness += it->getFitness();
 			it->setCurrentAction(Idle);
+            pos++;
         }
 
         float food = totalFitness * totalDuration;
