db37c32cd8b1fc35999f2d5e20864d4938623f53
microwav3s <microwav3s@googlemail.com>
2017-04-09 02:02:30 +0200

    Add modularity for event creation
    - Constructor only reads event modifiers that are explicitely mentioned
    in the event file; no exceptions thrown if modifier isn't mentioned

--- src/event.cpp
+++ src/event.cpp
@@ -1,7 +1,10 @@
 #include "event.h"
 
 Event::Event(std::string path) {
-    std::ifstream in(path);
+    //std::ifstream in(path);
+    std::ifstream in;
+    in.open("assets/events/000-test.json", std::ios::in);
+
     if (in.good()) {
         json data;
         in >> data;
@@ -10,14 +13,29 @@ Event::Event(std::string path) {
         title = data["title"];
         description = data["description"];
 
-        trigger = { ETechs::Handaxe, data["trigger"]["tribeSize"], data["trigger"]["tribeFood"], data["trigger"]["tribeMaterial"] };
 
-        for (auto& it : data["option"]) {
-            std::unique_ptr<Option> option;
-            option->name = data["option"]["name"];
-            option->effects.foodGain = data["option"]["food"];
-            option->effects.materialGain = data["option"]["materials"];
-            //Add other effects to option
+        if (!data["trigger"]["techResearched"].empty()) trigger.techResearched = data["trigger"]["techResearched"];
+        if (!data["trigger"]["tribeFood"].empty()) trigger.tribeFood = data["trigger"]["tribeFood"];
+        if (!data["trigger"]["tribeSize"].empty()) trigger.tribeSize = data["trigger"]["tribeSize"];
+        if (!data["trigger"]["tribeMaterial"].empty()) trigger.tribeMaterial = data["trigger"]["tribeMaterial"];
+
+        for (auto& it : data["options"]) {
+        std::unique_ptr<Option> option(new Option{ "", {0.0, 0.0, 0.0, 0.0, 0, 0.0, 0, 0, 0, 0, false } });
+            option->name = it["name"];
+            if (!it["huntBonus"].empty()) option->effects.huntBonus = it["huntBonus"];
+            if (!it["gatheringBonus"].empty()) option->effects.gatheringBonus = it["gatheringBonus"];
+            if (!it["fitnessGain"].empty()) option->effects.fitnessGain = it["fitnessGain"];
+            if (!it["intelligenceGain"].empty()) option->effects.intelligenceGain = it["intelligenceGain"];
+            if (!it["capacityGain"].empty()) option->effects.capacityGain = it["capacityGain"];
+            if (!it["foodGain"].empty()) option->effects.foodGain = it["foodGain"];
+            if (!it["materialGain"].empty()) option->effects.materialGain = it["materialGain"];
+
+            for (auto& caveman : it["cavemen"]) {
+                option->effects.new_age = caveman["age"];
+                option->effects.new_fitness = caveman["fitness"];
+                option->effects.new_intelligence = caveman["intelligence"];
+                option->effects.new_isMale = caveman["isMale"];
+            }
 
             options.push_back(std::move(option));
         }
