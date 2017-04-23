6915696512bda8c65fa53fed79abf7c090d08fb2
yuki <yuki77x@googlemail.com>
2017-04-23 15:00:27 +0200

    Fix duration and resources of improve

--- src/improve.cpp
+++ src/improve.cpp
@@ -1,23 +1,33 @@
 #include "improve.h"
 
+#include <cmath>
+
 Improve::Improve(short time) : Action(time) {
     type = EActions::ImproveAction;
 }
 
-void Improve::addActor(std::shared_ptr<Caveman> newactor) {
-        actors.push_back(newactor);
+void Improve::addActor(std::shared_ptr<Caveman> newActor) {
+    actors.push_back(newActor);
+    totalDuration = 10 * std::pow(0.5f, actors.size() - 1.f);
 }
 
 ActionPackage Improve::resolve(Tech::StatBoosts bonuses) {
-    int materialCosts = actors.size() * 10;
-    currentDuration += actors.size();
+    currentDuration += 1;
 
     if (currentDuration >= totalDuration) {
         for (auto& it : actors) {
             it->setCurrentAction(Idle);
         }
         return { true, 0.f, 0, 1, false };
+
+    } else if (currentDuration == 1) {
+        // Substract material costs once at start
+        return { false, 0.f, -materialCosts, 0, false };
     }
 
-    return { false, 0.f, -materialCosts, 0, false };
+    return { false, 0.f, 0, 0, false };
+}
+
+int Improve::getMaterialCosts() {
+    return materialCosts;
 }
--- src/improve.h
+++ src/improve.h
@@ -6,11 +6,16 @@
 #include "action.h"
 
 class Improve : public Action {
+private:
+    int materialCosts = 10;
+
 public:
     Improve(short time);
 
-    void addActor(std::shared_ptr<Caveman> actor);
+    void addActor(std::shared_ptr<Caveman> newActor);
     ActionPackage resolve(Tech::StatBoosts bonuses);
+
+    int getMaterialCosts();
 };
 
 #endif
